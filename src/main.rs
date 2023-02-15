b0a0ac8d4e4325ee9312d183e9863510beff75b4
Gaurav Raj <techw803@gmail.com>
2023-02-15 22:27:45 +0530

    Pre-release completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use chrono::Datelike;
 use select::document::Document;
 use select::predicate::{Class, Name};
@@ -6,7 +7,20 @@ use reqwest::header;
 use prettytable::{Table, Row, Cell};
 use std::collections::HashMap;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// prints the events of next month
+    #[arg(short, long)]
+    next: bool,
+
+    /// prints all the events that are available
+    #[arg(short, long)]
+    all: bool,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
     let months = HashMap::from([
         (1, "Jan"),
         (2, "Feb"),
@@ -22,7 +36,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         (12, "Dec"),
     ]);
 
-    let month = months.get(&chrono::Local::now().month()).unwrap().to_string();
+    let month:String;// = chrono::Local::now().month();
+
+    if cli.next {
+        let month_num = chrono::Local::now().month();
+        month = months.get(&(month_num + 1)).unwrap().to_string();
+    } else {
+        month = months.get(&chrono::Local::now().month()).unwrap().to_string();
+    }
 
     let body_result = get_data();
     let body = match body_result {
@@ -46,17 +67,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // iterate through the rows of the table and extract the CTF information
     for row in table.find(Name("tr")).skip(1) {
-        let date = row.find(Name("td")).skip(1).next().unwrap().text();
-        if date.contains(&month) {
+        if cli.all {
             names.push(row.find(Name("a")).next().unwrap().text());
             dates.push(row.find(Name("td")).skip(1).next().unwrap().text());
             styles.push(row.find(Name("td")).skip(2).next().unwrap().text());
             locations.push(row.find(Name("td")).skip(3).next().unwrap().text().trim().to_owned());
             weights.push(row.find(Name("td")).skip(4).next().unwrap().text());
         } else {
-            continue;
+            let date = row.find(Name("td")).skip(1).next().unwrap().text();
+            if date.contains(&month) {
+                names.push(row.find(Name("a")).next().unwrap().text());
+                dates.push(row.find(Name("td")).skip(1).next().unwrap().text());
+                styles.push(row.find(Name("td")).skip(2).next().unwrap().text());
+                locations.push(row.find(Name("td")).skip(3).next().unwrap().text().trim().to_owned());
+                weights.push(row.find(Name("td")).skip(4).next().unwrap().text());
+            } else {
+                continue;
+            }
         }
-   }
+    }
 
     print_data(names, dates, styles, locations, weights);
 
